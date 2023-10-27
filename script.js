9ff1db2dca1cca15616add673f0045524ca5105d
project232 <147293332+project232@users.noreply.github.com>
2023-10-27 14:26:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 const randomImageElement = document.getElementById('random-image');
 const generateImageButton = document.getElementById('generate-image');
 
-generateImageButton.addEventListener('click', function() {
+generateImageButton.addEventListener('click', function () {
     // Determine the screen width
     const screenWidth = window.innerWidth;
 
     // Determine the image dimensions based on the screen width
     let imageWidth, imageHeight;
 
-    if (screenWidth <= 900) {
+    if (screenWidth <= 800) {
         // For small screens (e.g., mobile devices)
         imageWidth = 400;
-        imageHeight = 700;
+        imageHeight = 900;
     } else {
         // For larger screens
-        imageWidth = 1000;
-        imageHeight = 600;
+        imageWidth = 1200;
+        imageHeight = 800;
     }
 
     // Generate a random image with the adjusted dimensions
     fetch(`https://picsum.photos/${imageWidth}/${imageHeight}`)
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.url;
         })
-        .then(randomURL => {
+        .then((randomURL) => {
             // Update the image source
             randomImageElement.src = randomURL;
         })
-        .catch(error => {
+        .catch((error) => {
             console.log('Error:', error);
         });
 });
